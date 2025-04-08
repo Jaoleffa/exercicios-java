54476355eecc7f301493cfd3d2e883c6fdbef992
João Leffa <joaoleapolinario@gmail.com>
2025-04-07 21:12:00 -0300

    SellerDaoJDBC and DaoFactory implementados

---
+++ demo-dao-jdbc/src/model/dao/DepartmentDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+    public void insert(Department obj);
+    public void update(Department obj);
+    public void deleteById(Integer id);
+    public Department findById(Integer id);
+    List<Department> findAll();
+
+}
---
+++ demo-dao-jdbc/src/model/dao/SellerDao.java
@@ -0,0 +1,11 @@
+package model.dao;
+import model.entities.Seller;
+import java.util.List;
+
+public interface SellerDao {
+    public void insert(Seller obj);
+    public void update(Seller obj);
+    public void deleteById(Integer id);
+    public Seller findById(Integer id);
+    List<Seller> findAll();
+}
