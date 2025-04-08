10a5cd77f91b3b384fcb64eea5d2ed94db8b28d3
João Leffa <joaoleapolinario@gmail.com>
2025-04-07 21:32:48 -0300

    FindByAdd implementado

--- demo-dao-jdbc/src/Main.java
+++ demo-dao-jdbc/src/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -5,10 +7,10 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department dp = new Department(1, "Batata");
-        System.out.println(dp);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller = new Seller(1, "João", "joao@gmail.com", new Date(), 3000, dp);
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
     }
 }
